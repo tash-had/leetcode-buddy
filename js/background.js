628b5595d54d1c5efe5734326d47eb5ebd62f6ba
Tash-had Saqif <me@tash-had.com>
2025-09-26 18:43:39 -0400

    Update to v3

--- js/background.js
+++ js/background.js
@@ -2,24 +2,26 @@ function isValidPage(urlStr) {
     return urlStr && urlStr.indexOf("leetcode.com") > -1;
 }
 
-chrome.browserAction.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener((tab) => {
     setPoputState(tab.id, tab.url);
 });
+
 chrome.tabs.onActivated.addListener(function (info) {
     chrome.tabs.get(info.tabId, function (change) {
         setPoputState(info.tabId, change.url);
     });
 });
+
 chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
     setPoputState(tabId, tab.url);
 });
 
 function setPoputState(tabId, tabUrl) {
     if (isValidPage(tabUrl)) {
-        chrome.browserAction.setPopup({ tabId: tabId, popup: 'html/config.html' });
-        chrome.browserAction.setIcon({ tabId: tabId, path: 'img/icon.png' });
+        chrome.action.setPopup({ tabId: tabId, popup: 'html/config.html' });
+        chrome.action.setIcon({ tabId: tabId, path: 'img/icon.png' });
     } else {
-        chrome.browserAction.setPopup({ tabId: tabId, popup: 'html/invalid-webpage.html' });
-        chrome.browserAction.setIcon({ path: 'img/icon-disabled.png', tabId: tabId });
+        chrome.action.setPopup({ tabId: tabId, popup: 'html/invalid-webpage.html' });
+        chrome.action.setIcon({ path: 'img/icon-disabled.png', tabId: tabId });
     }
 }
